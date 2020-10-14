424c2949a01b46c839f49aa674becf741599f0b8
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2020-10-14 16:26:47 +0530

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -4,3 +4,24 @@
 # Check your logs on heroku to see if you were sucessfull
 
 echo "NOTICE: extras.sh is runnning"
+
+
+apt-get install -q -y \
+    trace* \
+    golang \
+    perl \
+    ruby-full \
+    clang \
+    apache2 \
+    openssh-server \
+    netcat \
+    tor \
+    firefox \
+    python3 \
+    python3-pip \
+    git \
+    make \
+    busybox \
+    build-essential \
+    nodejs \
+    npm \
