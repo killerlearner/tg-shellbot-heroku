5563ae6bdcecd4dca147f2e6210e8975a6a63b87
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2020-10-14 17:16:47 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+echo Configuring Bot....
 cat > config.json <<EOF
 {
     "authToken": "$BOT_TOKEN",
     "owner": $OWN_ID
 }
 
+echo Configuring RClone....
+mkdir -p $HOME/.config/rclone/
 echo $RCLONE_CONFIG | base64 -d > rclone.conf
+cp rclone.conf $HOME/.config/rclone/rclone.conf
