be969961ee266d3b9bcb6d7875a0ee0013867940
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2020-11-24 09:08:11 +0530

    Update install-additional.sh

--- install-additional.sh
+++ install-additional.sh
@@ -2,19 +2,16 @@ mkdir -p /tmp/install-additional
 cd /tmp/install-additional
 
 # Install Golang
-wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
-tar -xvf go1.15.5.linux-amd64.tar.gz
-mv go /usr/local
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-mkdir -p $GOPATH/bin
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+#wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+#tar -xvf go1.15.5.linux-amd64.tar.gz
+#mv go /usr/local
+#export GOROOT=/usr/local/go
+#export GOPATH=$HOME/go
+#mkdir -p $GOPATH/bin
+#export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 # Install python packages
-pip3 install sqlmap htmlement beautifulsoup4 \
-yara-python requests selenium youtube-dl pafy \
-pyrogram scapy cryptography \
-frida frida-tools spidy-web-crawler
+pip3 install sqlmap htmlement beautifulsoup4 yara-python requests selenium youtube-dl pafy pyrogram scapy cryptography frida frida-tools spidy-web-crawler
 
 # Install golang tools
 go get -u github.com/j3ssie/metabigor
