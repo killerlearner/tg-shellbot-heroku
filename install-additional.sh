7d2539832d38aaf5ba256318f99774f061527ea7
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2020-11-24 09:27:32 +0530

    Update install-additional.sh

--- install-additional.sh
+++ install-additional.sh
@@ -14,17 +14,17 @@ cd /tmp/install-additional
 pip3 install sqlmap htmlement beautifulsoup4 yara-python requests selenium youtube-dl pafy pyrogram scapy cryptography frida frida-tools spidy-web-crawler
 
 # Install golang tools
-go get -u github.com/j3ssie/metabigor
-go get -u github.com/ffuf/ffuf
-go get github.com/OJ/gobuster
-(go get github.com/Ullaakut/cameradar && \
+go get -v -u github.com/j3ssie/metabigor
+go get -v -u github.com/ffuf/ffuf
+go get -v github.com/OJ/gobuster
+(go get -v github.com/Ullaakut/cameradar && \
 cd $GOPATH/src/github.com/Ullaakut/cameradar && \
 cd cmd/cameradar && \
 go install)
 curl -s "https://raw.githubusercontent.com/liamg/gitjacker/master/scripts/install.sh" | bash
-go get github.com/haccer/subjack
+go get -v github.com/haccer/subjack
 go get -v -u github.com/rverton/webanalyze/cmd/webanalyze
 curl -s "https://raw.githubusercontent.com/liamg/scout/master/scripts/install.sh" | bash
-go get github.com/Ice3man543/hawkeye
-go get github.com/c-sto/gogitdumper
-go get github.com/naltun/eyes
+go get -v github.com/Ice3man543/hawkeye
+go get -v github.com/c-sto/gogitdumper
+go get -v github.com/naltun/eyes
